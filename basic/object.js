ddf44e0c41b445ccb19186071a650461968edd13
kushagra kaushal <kishukaushal2004@gmail.com>
2025-12-18 15:53:26 +0000

    objcommit

---
+++ basic/object.js
@@ -0,0 +1,11 @@
+//singleton object 
+const obj = new Object()
+//non singleton object
+const obj1 = {
+    name:"kush",
+    roll:313,
+    "add" : "jaipur"
+}
+console.log(obj1);
+console.log(obj);
+
