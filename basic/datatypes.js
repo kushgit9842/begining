3002835d60387bb77e9a879356270be18b642158
kushagra kaushal <kishukaushal2004@gmail.com>
2025-12-18 06:17:54 +0000

    doitttt

--- basic/datatypes.js
+++ basic/datatypes.js
@@ -1,2 +1,7 @@
 const sem=7
-console.log(`hello how are you all. i am in my  ${sem} sem right now `);
+//console.log(`hello how are you all. i am in my  ${sem} sem right now `);
+const num=400
+console.log(num);
+// neeche walla object created hai isiliye output in terminal key value pair me aarha hai 
+const numb=new Number(400)
+console.log(numb);
