69e306761da4e3e1abc29934aca4be5c5a0040db
kushagra kaushal <kishukaushal2004@gmail.com>
2025-12-18 11:59:48 +0000

    araaay

--- basic/datatypes.js
+++ basic/datatypes.js
@@ -1,7 +1,12 @@
 const sem=7
 //console.log(`hello how are you all. i am in my  ${sem} sem right now `);
 const num=400
-console.log(num);
+//console.log(num);
 // neeche walla object created hai isiliye output in terminal key value pair me aarha hai 
 const numb=new Number(400)
-console.log(numb);
+//console.log(numb);
+let dates = new Date()
+//console.log(dates.getDate());
+console.log(typeof(Array.from("kushagra  hitesh")));
+console.log(Array.from("kushagra  hitesh"));
+
